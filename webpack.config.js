f20016c8561ce335f8ecf09390340eff3994c0bb
Dara Carolan <daraclare@gmail.com>
2019-11-26 21:48:14 +0000

    update

--- webpack.config.js
+++ webpack.config.js
@@ -154,17 +154,23 @@ export default {
             loader: "image-webpack-loader",
             options: {
               mozjpeg: {
-                progressive: true
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
               },
               gifsicle: {
                 interlaced: false
               },
-              optipng: {
-                optimizationLevel: 4
-              },
-              pngquant: {
-                quality: "75-90",
-                speed: 3
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
               }
             }
           }
